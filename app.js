967148b9473555cb708edf520852163c22d433e8
Nikola <nikola8989@hotmail.com>
2019-04-10 01:30:43 +0200

    setting file arhitecture

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + '/public'));
+
+app.get("/", function(req, res){
+	res.render("landing");
+});
+
+app.listen(80, "192.168.0.20", function(){
+	console.log("Server started...");
+});
